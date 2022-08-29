80a61b821683449520da727a504ff28f45bb8c98
yukiy <yyamashi@redhat.com>
2022-08-29 23:44:18 +0900

    [Mod] Partial correction of installation procedure

--- molecule_testnode/python_bootstrap.sh
+++ molecule_testnode/python_bootstrap.sh
@@ -1,6 +1,5 @@
 #/bin/bash
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-cd ~/.pyenv && src/configure && make -C src && cd -
+curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
@@ -13,7 +12,9 @@ eval "$(pyenv init -)"
 
 pyenv install 3.10.6
 pyenv global 3.10.6
+
 pip3 install pipenv selinux
+echo 'export PIPENV_VENV_IN_PROJECT=1' >> ~/.bashrc
 
 cat << EOF >> ~/.bashrc
 if [ -f /root/.pyenv/shims/python3 ] && [ ! -f /usr/bin/python ]; then
