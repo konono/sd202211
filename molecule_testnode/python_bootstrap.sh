edbd88c352291794eb99765fa7afa114f4d689f1
kon0n0 <kono@ms1.kononet.jp>
2023-05-09 23:12:03 +0900

    Update python_bootstrap.sh

--- molecule_testnode/python_bootstrap.sh
+++ molecule_testnode/python_bootstrap.sh
@@ -1,9 +1,5 @@
 #/bin/bash
 curl https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
@@ -14,13 +10,23 @@ pyenv install 3.10.6
 pyenv global 3.10.6
 
 pip3 install pipenv selinux
-echo 'export PIPENV_VENV_IN_PROJECT=1' >> ~/.bashrc
 
-cat << EOF >> ~/.bashrc
+cat << "EOL" >> ~/.bashrc
+export PIPENV_VENV_IN_PROJECT=1
+
+## Set path for pyenv
+export PYENV_ROOT="${HOME}/.pyenv"
+
+if [ -d "${PYENV_ROOT}" ]; then
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init - --no-rehash)"
+fi
 if [ -f /root/.pyenv/shims/python3 ] && [ ! -f /usr/bin/python ]; then
   ln --symbolic /root/.pyenv/shims/python3 /usr/bin/python3;
   ln --symbolic /root/.pyenv/shims/python /usr/bin/python;
 fi
-EOF
+EOL
+
 source ~/.bashrc
 pip3 install --upgrade pip
